0f01337ba5f15eab2d5a2d8bfb7515b238ff21cb
clermont80 <mclermont@la-providence.net>
2020-11-19 10:40:49 +0100

    recherche des cellules adjacentes effectuee

--- PathfindingLib/Pathfinder.cpp
+++ PathfindingLib/Pathfinder.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include "Pathfinder.h"
 #include <cstdlib>
+#include <limits>
+#include <math.h>
 
 
 using namespace tw;
@@ -25,6 +27,12 @@ Pathfinder::~Pathfinder()
 }
 
 
+
+int Pathfinder::TotalCostFromStartToEnd() //calcul du cout total du point de depart au point d'arrivee (heuristique)
+{
+	
+}
+
 Orientation Pathfinder::getOrientationFromPosition(Position p1, Position p2)
 {
 	// TODO : Implémenter le calcul de l'orientation du personnage à partir des positions passées en paramètres.
@@ -37,6 +45,97 @@ std::vector<Position> Pathfinder::getPath(Position startPosition, Position endPo
 {
 	std::vector<Position> path;
 
+	std::vector<CellData*> P;
+	std::map<CellData*, float> d;
+	
+
+	for (int i = 0; i < environment->getWidth(); i++)
+	{
+		for (int j = 0; j < environment->getHeight(); j++)
+		{
+			CellData * cell = environment->getMapData(i, j);
+			Obstacle* cell_target;
+
+
+			if (!cell->getIsObstacle() && cell_target->getIsTargettable)
+			{
+				d[cell] = std::numeric_limits<float>::max(); //sommet à +infini
+			}
+		}
+	}
+
+	d[environment->getMapData(startPosition.getX(), startPosition.getY())] = 0;
+	CellData * cell;
+	float PlusCourteDistance = std::numeric_limits<float>::max();
+	float NewDistance;
+
+	while(P.size != d.size)	//on itère tant que qu'il existe un sommet hors de P donc tant que P ne fait pas la même taille que le nombre total de sommets
+	{
+		CellData * a = NULL;
+		for (int i = 0; i < environment->getWidth(); i++)
+		{
+			for (int j = 0; j < environment->getHeight(); j++)
+			{	
+				cell = environment->getMapData(i, j);
+				
+				if (std::find(P.begin(), P.end(), cell) == P.end()) //on verifie que le sommet n'est pas dans P
+				{
+					if (d[cell] < PlusCourteDistance)
+					{
+						PlusCourteDistance = d[cell];
+						a = cell;		//on ajoute la cellule à l'ensemble P car elle est parcourue
+					}
+				}
+			}
+		}
+
+		if (a != NULL)
+		{
+			P.push_back(a);
+			std::vector<CellData*> Voisins;
+
+
+			
+					if (d[cell + cell->getX] != environment->getWidth + 1) //adjacent droit
+					{
+						Voisins.push_back(cell + cell->getX);
+					}
+
+					if (d[cell - cell->getX] != -1)	//adjacent gauche
+					{
+						Voisins.push_back(cell - cell->getX);
+					}
+
+					if (d[cell + cell->getY] != environment->getHeight + 1)	//adjacent bas
+					{
+						Voisins.push_back(cell + cell->getY);
+					}
+
+					if (d[cell - cell->getY] != environment->getHeight + 1)	//adjacent haut
+					{
+						Voisins.push_back(cell - cell->getY);
+					}
+			
+			//comparaison des distances voisines
+
+			std::vector<float> distancesfroma;
+			bool sorted = false;
+
+			for (int i = 0; i < Voisins.size; i++)
+			{
+				
+				distancesfroma[i] = sqrt((Voisins[i]->getX - a->getX)*(Voisins[i]->getX - a->getX)+(Voisins[i]->getY - a->getY)*(Voisins[i]->getY - a->getY)); //formule pour calculer une distance entre deux cellules
+				
+			}
+
+			
+		}
+
+	}
+
+	
+
+	
 	// TODO : Implémenter un algorithme de recherche de plus court chemin entre startPosition et endPosition
 	// qui tient compte de la topologie de l'environnement (environment : case "non marchable"; obstacles, etc...)
 	// et des obstacles "dynamique" (les personnages, etc...)

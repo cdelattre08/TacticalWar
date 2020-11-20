98bf5a94562ae78f6209a92eb2742fe2608c9fbe
valentin_bouet <valbouet@outlook.fr>
2020-11-20 16:05:23 +0100

    colorisation

--- ColorisationTestModule/main.cpp
+++ ColorisationTestModule/main.cpp
@@ -1,25 +1,91 @@
 
 #include <vector>
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
+using namespace std;
 
 int main(int argc, char** argv)
 {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Coloration test module");
 
+	sf::Texture Source;
+	sf::Texture Assets;
+	if (!Source.loadFromFile("assets/Warrior/bottomright-die-sheet.png")) { std::cout << "Impossible de charger Grass texture" << std::endl; }
+	if (!Assets.loadFromFile("assets/Warrior/mask/bottomright-die-sheet.png")) { std::cout << "Impossible de charger Grass texture" << std::endl; }
+	sf::Sprite perso;
+	sf::Uint8  *pixels = new sf::Uint8[800 * 600 * 4];
+
+	// Passage en image pour accéder aux pixels :
+	sf::Image src = Source.copyToImage();
+	sf::Image img = Assets.copyToImage();
+
+	perso.setTexture(Source);
+	Source.setSmooth(true);
+
+	// Couleur que l'on souhaite appliquer :
+	sf::Color toApply1 = sf::Color(100,100,100);
+	sf::Color toApply2 = sf::Color(201, 201, 201);
+	sf::Color toApply3 = sf::Color(232,220,198);
+	
+	for (int x = 0; x < img.getSize().x; x++)
+	{
+		for (int y = 0; y < img.getSize().y; y++)
+		{
+			sf::Color couleur = img.getPixel(x, y);
+
+			if (couleur.r > couleur.b && couleur.r > couleur.g && couleur.r > 50)
+			{
+				sf::Color colorSrc = src.getPixel(x, y);
+				colorSrc = colorSrc * toApply1;
+				src.setPixel(x, y, colorSrc);
+
+			}else if (couleur.b > couleur.r && couleur.b > couleur.g && couleur.b > 50)
+			{
+				sf::Color colorSrc = src.getPixel(x, y);
+				colorSrc = colorSrc * toApply2;
+				src.setPixel(x, y, colorSrc);
+
+			}else if (couleur.g > couleur.r && couleur.g > couleur.b && couleur.g > 50)
+			{
+				sf::Color colorSrc = src.getPixel(x, y);
+				colorSrc = colorSrc * toApply3;
+				src.setPixel(x, y, colorSrc);
+			}
+			
+			
+
+			/*sf::Color couleur = img.getPixel(x, y);
+			couleur = couleur * toApply;
+			img.setPixel(x, y, couleur);*/
+
+			//pixels[(y * x) * 4] = 255; // R?
+			//pixels[(y * x) * 4 + 1] = 255; // G?
+			//pixels[(y * x) * 4 + 2] = 255; // B?
+			//pixels[(y * x) * 4 + 3] = 255; // A?
+		}
+	}
+
+	Source.update(src);
+
 	while (window.isOpen())
 	{
+		
+
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
+			
+
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-
+		
 		window.clear();
-
+		window.draw(perso);
 		window.display();
 	}
 
+	delete[] pixels;
 	return 0;
 }
